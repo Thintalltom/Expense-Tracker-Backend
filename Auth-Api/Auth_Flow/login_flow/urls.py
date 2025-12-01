2987bd188579c6651e72393709b761c9b6f7eb5d
Tomide Adeyanju <tadeyanju@infinion.co>
2025-12-01 16:08:24 +0100

    added repath

--- Auth-Api/Auth_Flow/login_flow/urls.py
+++ Auth-Api/Auth_Flow/login_flow/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path 
+from django.urls import path, re_path
 from . import views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -27,7 +27,7 @@ urlpatterns = [
 #     path('products/', views.get_products, name='products'),
 #    path('products/<int:pk>/', views.product_detail, name='product-detail'),
     #swagger-UI
-    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
